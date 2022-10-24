80dfa1f0c007af53b8f0178586b5a5ab2514f29b
Elanakova Liubov <liubov.elanakova@posti.com>
2022-10-24 16:45:48 +0300

    computer guessing function is added

--- guess_the_number.py
+++ guess_the_number.py
@@ -13,4 +13,21 @@ def guess_the_number(x):
     print(f"Congrats, you have guessed the number {random_number}")
 
 
-guess_the_number(10)
+def computer_guess_the_number(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low
+        feedback = input(f'Is {guess} too high(H), too low(L) or correct(C)? ').lower()
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+
+    print(f"The computer guessed your number {guess} correctly!")
+
+computer_guess_the_number(1000)
